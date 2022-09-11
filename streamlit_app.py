cef773a767015b548e5d5d6f14a93a11ce483ae2
ankushbalpande <85275931+ankushbalpande@users.noreply.github.com>
2022-09-11 16:24:55 -0400

    Update streamlit_app.py
    emoji

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,7 @@
 import streamlit
 streamlit.title("My parent's new healty Dinner")
-streamlit.header('Breakfast Menu')
+streamlit.header('Breakfast Menu 🥣 🥗 🐔 🥑🍞')
 streamlit.text('Omega 3 & Blueberry Oatmeal')
 streamlit.text('Kale, Spinach & Rocket Smoothie')
 streamlit.text('Hard-Boiled Free-Range Egg')
-🥣 🥗 🐔 🥑🍞
+
